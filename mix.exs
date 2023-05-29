e0fbe944a7b060a996e7e47e070a3e617f086f55
Conrad Taylor <conradwt@gmail.com>
2023-05-29 14:11:10 -0700

    Update to Elixir 1.15.0-rc.1. (#15)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Rng.MixProject do
     [
       app: :rng,
       version: "0.1.0",
-      elixir: "~> 1.15.0-rc.0",
+      elixir: "~> 1.15.0-rc.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
