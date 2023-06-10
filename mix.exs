0f5d9cb0361d89023abc75a5b4078dc853bb9d27
Conrad Taylor <conradwt@gmail.com>
2023-06-09 22:24:51 -0700

    Refresh-hex-packages (#18)

--- mix.exs
+++ mix.exs
@@ -33,13 +33,13 @@ defmodule Rng.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.7.3"},
-      {:phoenix_live_dashboard, "~> 0.7.2"},
+      {:phoenix_live_dashboard, "~> 0.8.0"},
       {:telemetry_metrics, "~> 0.6.1"},
       {:telemetry_poller, "~> 1.0.0"},
-      {:gettext, "~> 0.22.1"},
+      {:gettext, "~> 0.22.2"},
       {:jason, "~> 1.4.0"},
-      {:bandit, "~> 1.0-pre.4"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:bandit, "~> 1.0.0-pre.6"},
+      {:credo, "~> 1.7.0", only: [:dev, :test], runtime: false}
     ]
   end
 
