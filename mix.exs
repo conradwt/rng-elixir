85e87cceddc99dba92f2c67b0cf3b69874d60006
Conrad Taylor <conradwt@gmail.com>
2023-06-28 00:54:52 -0700

    Update to Phoenix 1.7.6. (#23)

--- mix.exs
+++ mix.exs
@@ -32,13 +32,13 @@ defmodule Rng.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.3"},
+      {:phoenix, "~> 1.7.6"},
       {:phoenix_live_dashboard, "~> 0.8.0"},
       {:telemetry_metrics, "~> 0.6.1"},
       {:telemetry_poller, "~> 1.0.0"},
       {:gettext, "~> 0.22.2"},
       {:jason, "~> 1.4.0"},
-      {:bandit, "~> 1.0.0-pre.6"},
+      {:bandit, "~> 1.0.0-pre.9"},
       {:credo, "~> 1.7.0", only: [:dev, :test], runtime: false}
     ]
   end
