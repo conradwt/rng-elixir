7c9038cdacc8712d1a97ccd147d267bc998650b7
Conrad Taylor <conradwt@gmail.com>
2023-06-29 22:44:28 -0700

    Update to Erlang 26.0.2 and refresh Hex packages.

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Rng.MixProject do
       {:telemetry_poller, "~> 1.0.0"},
       {:gettext, "~> 0.22.2"},
       {:jason, "~> 1.4.0"},
-      {:bandit, "~> 1.0.0-pre.9"},
+      {:bandit, "~> 1.0.0-pre.10"},
       {:credo, "~> 1.7.0", only: [:dev, :test], runtime: false}
     ]
   end
