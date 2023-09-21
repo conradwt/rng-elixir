8cbac77fdcd506498c2b44ff470b5a248d25db59
Conrad Taylor <conradwt@gmail.com>
2023-09-20 23:24:24 -0700

    Update to Elixir 1.15.6. (#48)

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Rng.MixProject do
       {:telemetry_poller, "~> 1.0.0"},
       {:gettext, "~> 0.22.3"},
       {:jason, "~> 1.4.1"},
-      {:bandit, "~> 1.0.0-pre.15"},
+      {:bandit, "~> 1.0.0-pre.16"},
       {:credo, "~> 1.7.0", only: [:dev, :test], runtime: false}
     ]
   end
