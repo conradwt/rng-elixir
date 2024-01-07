d2c6bf6145f101b79b0d53280993ddddb7351701
Conrad Taylor <conradwt@gmail.com>
2024-01-07 03:16:43 -0800

    Update Hex packages.
    * Update Hex packages.

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Rng.MixProject do
       {:gettext, "~> 0.22.3"},
       {:jason, "~> 1.4.1"},
       {:bandit, "~> 1.0.0"},
-      {:credo, "~> 1.7.2", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7.3", only: [:dev, :test], runtime: false}
     ]
   end
 
