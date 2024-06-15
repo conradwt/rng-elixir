96cb61e94bc82626132017b5aa6e2acc5d3c9496
Conrad Taylor <conradwt@gmail.com>
2024-06-15 12:37:24 -0700

    Update to Elixir 1.17.0. (#90)

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Rng.MixProject do
       {:gettext, "~> 0.22.3"},
       {:jason, "~> 1.4.1"},
       {:bandit, "~> 1.3.0"},
-      {:credo, "~> 1.7.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7.7", only: [:dev, :test], runtime: false}
     ]
   end
 
