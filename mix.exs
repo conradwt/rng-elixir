ceccca49b206cd818534281ca4c7f7c9c82c8c6b
Conrad Taylor <conradwt@gmail.com>
2025-06-22 00:21:52 -0700

    Update to Erlang 28.0.1.

--- mix.exs
+++ mix.exs
@@ -9,8 +9,7 @@ defmodule Rng.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps(),
-      compilers: [:yecc] ++ Mix.compilers()
+      deps: deps()
     ]
   end
 
