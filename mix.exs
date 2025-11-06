7cf10bc839763da7c7cd6ec6af17e7cad5ad3bf0
Conrad Taylor <conradwt@gmail.com>
2025-11-06 01:50:17 -0800

    Update system requirements. (#129)
    * Update system requirements.
    
    * Merge remote-tracking branch 'origin/main' into update-system-requirements

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Rng.MixProject do
     [
       app: :rng,
       version: "0.2.0",
-      elixir: "~> 1.18.4",
+      elixir: "~> 1.19.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
