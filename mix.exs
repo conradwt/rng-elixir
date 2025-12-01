b9bf055507c0f93ac779dd5229d3f575c3bf6094
Conrad Taylor <conradwt@gmail.com>
2025-11-30 18:51:22 -0800

    Update system requirements.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Rng.MixProject do
     [
       app: :rng,
       version: "0.2.0",
-      elixir: "~> 1.19.3",
+      elixir: "~> 1.19.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
